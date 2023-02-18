1b315ceb3ef5a3073e400086b317c5f01092a23c
Dipjyoti Metia <dipjyotimetia@gmail.com>
2023-02-19 01:23:07 +1100

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(/usr/bin/wait-for localhost:${PUBSUB_PORT} --timeout=0 PUBSUB_EMULATOR_HOST=localhost:${PUBSUB_PORT} /usr/bin/pubsub-emulator -debug; nc -lkp 8682 >/dev/null) &
+(/usr/bin/wait-for localhost:${PUBSUB_PORT} --timeout=0 --PUBSUB_EMULATOR_HOST=localhost:${PUBSUB_PORT} /usr/bin/pubsub-emulator; nc -lkp 8682 >/dev/null) &
 
 
-gcloud beta emulators pubsub start --project=${PUBSUB_PROJECT} --host-port=0.0.0.0:${PUBSUB_PORT} "$@"
+gcloud beta emulators pubsub start --host-port=0.0.0.0:${PUBSUB_PORT} --project=${PUBSUB_PROJECT} "$@"
