2745757a017d3fffb4d7d1d7debbcf6e8f84600e
Dipjyoti Metia <dipjyotimetia@gmail.com>
2023-02-22 15:11:23 +1100

    updated with fix

---
+++ README.md
@@ -0,0 +1,24 @@
+# Google Cloud PubSub Emulator
+
+[Google Cloud PubSub Emulator](https://cloud.google.com/pubsub/docs/emulator)
+
+## Usage
+
+Capability to create multiple topic and subscription
+
+Set the `PUBSUB_PROJECT`, `PUBSUB_TOPIC`, `PUBSUB_SUBSCRIPTION` and `PUBSUB_PORT` environment variables when running the image.
+
+```yaml
+version: '3.8'
+services:
+  pubsub:
+    build: .
+    environment:
+      - PUBSUB_PROJECT=test-project
+      - PUBSUB_TOPIC=test-topic1,test-topic2,test-topic3
+      - PUBSUB_SUBSCRIPTION=test-sub1,test-sub2,test-sub3
+      - PUBSUB_PORT=8085
+```
+
+---
+Thanks to [RoryQ/spanner-emulator](https://github.com/RoryQ/spanner-emulator) for the motivation.
--- main.go
+++ main.go
@@ -3,51 +3,52 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 	"time"
 
 	"cloud.google.com/go/pubsub"
 )
 
 var (
-	projectID    = os.Getenv("PUBSUB_PROJECT")
-	topicID      = os.Getenv("PUBSUB_TOPIC")
-	subscription = os.Getenv("PUBSUB_SUBSCRIPTION")
+	projectID = os.Getenv("PUBSUB_PROJECT")
+	topicIDS  = os.Getenv("PUBSUB_TOPIC")
+	subIDS    = os.Getenv("PUBSUB_SUBSCRIPTION")
 )
 
 func main() {
 	ctx := context.Background()
-	createTopic(ctx)
-	createSubscription(ctx)
+	createTopicSubscription(ctx)
 }
 
-func createTopic(ctx context.Context) {
+func createTopicSubscription(ctx context.Context) {
+	topics := strings.Split(topicIDS, ",")
+	subscriptions := strings.Split(subIDS, ",")
+
 	client, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
 		_ = fmt.Errorf("pubsub.NewClient: %v", err)
 	}
 	defer client.Close()
 
-	t, err := client.CreateTopic(ctx, topicID)
-	if err != nil {
-		_ = fmt.Errorf("CreateTopic: %v", err)
+	if len(topics) == len(subscriptions) {
+		for i := 0; i < len(topics); i++ {
+			t, err := client.CreateTopic(ctx, topics[i])
+			if err != nil {
+				_ = fmt.Errorf("failed to create topic: %v", err)
+			}
+			log.Printf("topic created: %v\n", t)
+			sub, err := client.CreateSubscription(ctx, subscriptions[i], pubsub.SubscriptionConfig{
+				Topic:       client.Topic(topics[i]),
+				AckDeadline: 20 * time.Second,
+			})
+			if err != nil {
+				_ = fmt.Errorf("failed to create subscription: %v", err)
+			}
+			log.Printf("created subscription: %v\n", sub)
+		}
+	} else {
+		_ = fmt.Errorf("number of topic and subscription are not same")
 	}
-	fmt.Printf("Topic created: %v\n", t)
-}
-
-func createSubscription(ctx context.Context) {
-	client, err := pubsub.NewClient(ctx, projectID)
-	if err != nil {
-		_ = fmt.Errorf("pubsub.NewClient: %v", err)
-	}
-	defer client.Close()
-
-	sub, err := client.CreateSubscription(ctx, subscription, pubsub.SubscriptionConfig{
-		Topic:       client.Topic(topicID),
-		AckDeadline: 20 * time.Second,
-	})
-	if err != nil {
-		_ = fmt.Errorf("CreateSubscription: %v", err)
-	}
-	fmt.Printf("Created subscription: %v\n", sub)
 }
